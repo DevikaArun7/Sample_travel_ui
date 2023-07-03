2a29c98d16d9a0c0f262dcf88ab4a59f84abe0fa
Devika Arun <devikaaruntv@gmail.com>
2023-07-03 20:14:10 +0530

    homepage completed,slider completed

--- lib/Screens/homepage.dart
+++ lib/Screens/homepage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sample_ui/Screens/drawer.dart';
+import 'package:sample_ui/Screens/slider.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -15,6 +16,44 @@ class _HomePageState extends State<HomePage> {
       backgroundColor: const Color(0xFFC0CECE),
       drawer: const NavBar(),
       appBar: AppBar(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+             const SizedBox(
+              height: 197, // Adjust the height as needed
+              child: SliderScreen(),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(45.0),
+              child: Container(
+                width: double.infinity,
+                child: const Text("       We Fulfill \n               YourTravel \n                         Dreams",style: TextStyle(fontSize: 23,fontStyle: FontStyle.italic,shadows: [Shadow(color: Colors.blue,blurRadius: 2.0,offset: Offset(2.0, 2.0))]),)),
+            ),
+            const Align(alignment: AlignmentDirectional.center,
+            child: Text("Popular Packages",style:TextStyle(fontSize: 23,fontWeight: FontWeight.bold),
+            
+            ),
+            ),
+           Column(
+             children: [
+               const SizedBox(
+             height: 197, 
+             // Adjust the height as needed
+             child: SliderScreen(),
+           ),
+             ],
+           ),
+           Padding(
+             padding: const EdgeInsets.all(13.0),
+             child: ElevatedButton(onPressed: (){}, child: Text("Our Packages"),),
+           ),
+          ],
+          
+        ),
+        
+      ),
+      
     );
   }
 }
+
